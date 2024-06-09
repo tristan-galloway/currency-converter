d168f991dc8f58c6a594a2733eff4108d638cf4b
Tristan Galloway <tristan.a.galloway@gmail.com>
2024-06-08 20:51:15 -0600

    Finished first sprint on project. Everything is functioning.

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 // Create Global Variables
-let data = '';
+let data;
+let input = document.getElementById('in');
+let output = document.getElementById('out');
 
 // Creates a object of the JSON data
 async function readData() {
@@ -30,51 +32,49 @@ function createOptions (data, targetID) {
     }
 };
 
-// // Takes the input and converts it to USD
-function inputCurrency(qty, key, data) {
-    try {
-            if (key == 'usd') {
-                return qty;
-            } else {
-                const exchangeRate = data[key].inverseRate; // Gets exchange rate for the key that was passed in
-                const usd = qty * exchangeRate; // Calculates the USD equivilant
-                return usd;
-            }
-        } catch (error) {
-            console.error(error); // Handle errors
-    }
-}
-
-function outputCurrency(usd, key, data) {
-    try {
-            const exchangeRate = data[key].rate; // Gets the exchange rate for the current currency
-            const otherCurrency = usd * exchangeRate; // Converts USD to the desired currency.
-            return otherCurrency;
-        } catch (error) {
-            console.error(error); // Handle errors
-    }
-}
-
 function convertCurrency (event) {
     if (event.target === input) {
-        // Get the current key and the desired key.
-        let inputKey = document.getElementById('input').selectedOption.value;
-        let outputKey = document.getElementById('output').selectedOption.value;
-        // Get the qty and rate of the current key.
-        let qty = event.target.text;
-        // Get the current rate and the inverse rate of the desired currency.
-        let rate = data[inputKey].rate
-        let inverseRate = data[outputKey].inverseRate
-        // Get the location to put the converted value.
+        // Get the current key and the desired key and target.
+        let inputKey = document.getElementById('input').value;
+        let outputKey = document.getElementById('output').value;
         let target = document.getElementById('out');
-        if (inputKey == 'usd') {
-            target.text = qty;
+
+        // Get the qty and rate of the current key.
+        let qty = event.target.value;
+
+        // Get the current rate and the inverse rate of the desired currency.
+        let rate = data[inputKey].rate;
+        let inverseRate = data[outputKey].inverseRate;
+        let usd = qty * rate;
+        let endCurrency = (usd * inverseRate);
+
+        // Update the target with the converted value.
+        if (outputKey == 'usd') {
+            target.value = usd.toFixed(2);
         } else {
-            let usd = qty * rate;
-            target.text = usd * inverseRate;
+            target.value = endCurrency.toFixed(2);
         }
     } else if (event.target == output) {
+        // Get the current key and the desired key and target
+        let inputKey = document.getElementById('output').value;
+        let outputKey = document.getElementById('input').value;
+        let target = document.getElementById('in');
 
+        // Get the qty and rate of the current key.
+        let qty = event.target.value;
+    
+        // Get the current rate and the inverse rate of the desired currency.
+        let rate = data[inputKey].rate;
+        let inverseRate = data[outputKey].inverseRate;
+        let usd = qty * rate;
+        let endCurrency = (usd * inverseRate);
+
+        // Update the target with the converted value.
+        if (inputKey == 'usd') {
+            target.value = usd.toFixed(2);
+        } else {
+            target.value = endCurrency.toFixed(2);
+        }
     };
 };
 
@@ -93,9 +93,6 @@ async function main() {
 // Call the main async function
 main();
 
-let input = document.getElementById('in');
-let output = document.getElementById('out');
-
 // Event Listeners
-input.addEventListener('change', convertCurrency)
-output.addEventListener('change', convertCurrency)
+input.addEventListener('keyup', convertCurrency)
+output.addEventListener('keyup', convertCurrency)
